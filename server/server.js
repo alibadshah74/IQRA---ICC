46b2f02a9c9fb6915725f30c2a2427ad6bd5c7fb
Delta Student <mdmajidhussain1106@gmail.com>
2026-02-12 11:59:31 +0530

    export app

--- server/server.js
+++ server/server.js
@@ -49,4 +49,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 //to start our application
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+//app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+export default app;
